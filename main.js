119ec5e0a17c16ea2391c24920ce9796a693bc32
ccp84 <ccp84@icloud.com>
2022-12-16 11:10:20 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,47 @@
+let userName = "";
+
+userName ? console.log(`Hello, $username`) : console.log("Hello");
+
+let userQuestion = "This is a question";
+console.log(userQuestion);
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = "";
+
+switch (randomNumber) {
+  case 1:
+    console.log("It is certain");
+    break;
+
+  case 2:
+    console.log("It is decidedly so");
+    break;
+
+  case 3:
+    console.log("Reply hazy try again");
+    break;
+
+  case 4:
+    console.log("Cannot predict now");
+    break;
+
+  case 5:
+    console.log("Do not count on it");
+    break;
+
+  case 6:
+    console.log("My sources say no");
+    break;
+
+  case 7:
+    console.log("Outlook not so good");
+    break;
+
+  case 8:
+    console.log("Signs point to yes");
+    break;
+
+  default:
+    console.log("Try again later");
+}
